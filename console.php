13b822bef18812492af83528576db133a69e4014
Thiago Miranda <tamsmiranda@gmail.com>
2012-08-16 22:44:19 -0300

    Primeiro commit usando Symfony

---
+++ console.php
@@ -0,0 +1,8 @@
+<?php
+// console.php
+require_once __DIR__.'/src/autoload.php';
+
+use Symfony\Component\Console as Console;
+
+$application = new Console\Application('Demo', '1.0.0');
+$application->run();
